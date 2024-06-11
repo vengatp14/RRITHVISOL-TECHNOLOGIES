89f189df65af1c72edcde252b0003a3be9d270ae
Deepakvsdev <deepak.vs@softwaredevelopmentandgraphicdesign.tech>
2024-06-11 19:31:24 +0530

    bug fixing done by client changes

---
+++ static/js/owl.corosel.js
@@ -0,0 +1,44 @@
+ 
+ (function($) {
+    "use strict";
+ 
+ // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 25,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+        '<i class="bi bi-chevron-left"></i>',
+        '<i class="bi bi-chevron-right"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        992: {
+            items: 2
+        }
+    }
+});
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+  })
+  })
+ });(jquery);
